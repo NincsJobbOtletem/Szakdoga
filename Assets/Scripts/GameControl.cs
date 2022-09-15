e56a3b836b823fe2185f397ac3551c27441f9c96
NincsJobbOtletem <patrikvok123312@gmail.com>
2022-09-15 16:42:38 +0200

    Update basics

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -27,24 +27,17 @@ public class GameControl : MonoBehaviour
     private float accuracy;
 
     private int targetsAmount;
+
     private Vector2 targetRandomPosition;
-// Start is called before the first frame update
+    
     void Start()
-    { 
-    cursorHotspot = new Vector2(cursorTexture.width /2, cursorTexture.height / 2);
-    Cursor.SetCursor(cursorTexture, cursorHotspot, CursorMode.Auto);
-    getReadyText.gameObject.SetActive(false);
-    targetsAmount = 50;
-    score = 0;
-    shotsFired = 0;
-    targetsHit = 0;
-    accuracy = e1;
-}
-// Update is called once per frame
-// Update is called once per frame
-void Update()
-{
-if (Input.GetMouseButtonDown(0))
-{
-shotsFired += 1f; I
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
